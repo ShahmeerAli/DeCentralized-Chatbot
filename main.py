2140a5dbe2b0ee16a3da8c52b272a3758ac8b010
ShahmeerAli <shahmeer.devel@gmail.com>
2025-09-06 22:44:00 +0500

    addded nodes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from langchain_groq import ChatGroq
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph,END,add_messages
 from langchain_core.messages import AIMessage,HumanMessage,SystemMessage
 from langchain_community.tools import TavilySearchResults
 from dotenv import find_dotenv,load_dotenv
+from typing import Annotated,TypedDict
 
 
 load_dotenv()
+
 
 
 GROQAPI_KEY=os.environ["GROQAPI_KEY"]
@@ -15,6 +17,39 @@ llm=ChatGroq(
  model="llama3-70b-8192",
  groq_api_key=os.environ.get("GROQAPI_KEY")
 )
+
+search_tools=TavilySearchResults()
+
+tools=[search_tools]
+
+class AgentState(TypedDict):
+    messages:Annotated[list,add_messages]
+
+
+
+async def answer_generator(state:AgentState):
+    result=await llm.ainvoke(tools=tools)
+    return{
+       'messages':state['messages'] + [result]
+    }
+
+
+async def revisor(state:AgentState):
+    """Revise the Answer."""
+    result=await llm.ainvoke(
+           tools=tools,
+           SystemMessage=""
+       )
+      
+
+async def critique(state:AgentState):
+    """Critque the answer"""
+
+async def final_answer(state:AgentState):
+    pass
+
+    
+
 
 
 
