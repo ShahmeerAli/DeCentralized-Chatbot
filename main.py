01b05e9d547f616419c5e5a63ebbab6a0e414da6
ShahmeerAli <shahmeer.devel@gmail.com>
2025-09-13 22:17:45 +0500

    langgraph complete, MCP remaining

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph,END,add_messages
 from langchain_core.messages import AIMessage,HumanMessage,SystemMessage
 from langchain_community.tools import TavilySearchResults
+from langgraph.prebuilt import ToolNode
 from dotenv import find_dotenv,load_dotenv
 from langchain_mcp import MCPToolkit
+from mcp.client.session import ClientSession
 from typing import Annotated,TypedDict
+import asyncio
 
 
 load_dotenv()
@@ -15,12 +18,16 @@ load_dotenv()
 GROQAPI_KEY=os.environ["GROQAPI_KEY"]
 
 llm=ChatGroq(
- model="llama3-70b-8192",
+ model="llama-3.1-8b-instant",
  groq_api_key=os.environ.get("GROQAPI_KEY")
 )
 
-mcp_toolkit=MCPToolkit.from_server("tavily-mcp")
-tools=mcp_toolkit.get_tools()
+
+search_tools=TavilySearchResults()
+tools=[search_tools]
+
+tool_node=ToolNode(tools=tools)
+
 
 
 class AgentState(TypedDict):
@@ -29,8 +36,7 @@ class AgentState(TypedDict):
 
 
 async def answer_generator(state:AgentState):
-    result=await llm.ainvoke(messages=state["messages"],
-        tools=tools)
+    result=await llm.ainvoke(state['messages'])
     
     return{
        'messages':state['messages'] + [result]
@@ -40,8 +46,7 @@ async def answer_generator(state:AgentState):
 async def revisor(state:AgentState):
     """Revise the Answer."""
     result=await llm.ainvoke(
-           tools=tools,
-           messages=[SystemMessage(content="You are a professional answer revisor."
+           state['messages'] + [SystemMessage(content="You are a professional answer revisor."
            "Use critique to rewrite or improve the polished version of the answer."
  )]           )
     return {
@@ -53,8 +58,7 @@ async def revisor(state:AgentState):
 async def critique(state:AgentState):
     """Critque the answer"""
     result=await llm.ainvoke(
-        tools=tools,
-        messages=[SystemMessage(content="You are a professional critique agent."
+        state['messages'] + [SystemMessage(content="You are a professional critique agent."
         "Read the message and critique it to find what's missing."
         "Suggest how the answer can be more refined,polished and sourced.")]
     )
@@ -68,8 +72,7 @@ async def critique(state:AgentState):
 async def final_answer(state:AgentState):
     """GIVES the final answer"""
     result=await llm.ainvoke(
-        tools=tools,
-        messages=[SystemMessage(content="You are the final arbiter."
+        state['messages']+[SystemMessage(content="You are the final arbiter."
         "Generate the answer."
         "Do not include critique remarks only generate the best polished answer.")]
         )
@@ -92,6 +95,8 @@ graph.add_node("answer_generator",answer_generator)
 graph.add_node("revisor",revisor)
 graph.add_node("critique",critique)
 graph.add_node("final_answer",final_answer)
+graph.add_node("tool",tool_node)
+
 
 #adding edges to connect the nodes
 
@@ -102,8 +107,16 @@ graph.add_edge("critique","final_answer")
 graph.set_entry_point("answer_generator")
 workflow=graph.compile()
 
+async def run():
+
+    response=await workflow.ainvoke({
+        'messages':[HumanMessage(content="what is tesla optimus?")]
+    })
 
 
+    print(response)
 
+
+asyncio.run(run())
 
 
